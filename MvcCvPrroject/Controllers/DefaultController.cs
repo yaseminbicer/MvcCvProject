c37802089a88e536b577d0f6346babe983d13c9a
Yasemin <yasemin.bicer@bahcesehir.edu.tr>
2024-03-01 02:39:09 +0300

    Add project files.

---
+++ MvcCvPrroject/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvPrroject.Models.Entity;
+
+namespace MvcCvPrroject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbCvEntities db = new dbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.tblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler =db.tblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.tblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.tblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.tblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+    }
+}
