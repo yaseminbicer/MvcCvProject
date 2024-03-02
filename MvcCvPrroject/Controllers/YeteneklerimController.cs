a34125caf62f73189a4c39565a4036431fe08a21
yaseminbicer <yasemin.bicer@bahcesehir.edu.tr>
2024-03-02 21:26:10 +0300

    Admin panelde, denyimler sayfası eklendi

---
+++ MvcCvPrroject/Controllers/YeteneklerimController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvPrroject.Models.Entity;
+using MvcCvPrroject.Repositories;
+namespace MvcCvPrroject.Controllers
+{
+    public class YeteneklerimController : Controller
+    {
+        // GET: Yeteneklerim
+        GenericRepository<tblYeteneklerim> repo= new GenericRepository<tblYeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();
+            return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(tblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
