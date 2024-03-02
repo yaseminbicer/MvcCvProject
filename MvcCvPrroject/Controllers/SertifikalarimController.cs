507361dbab60003eda16972ed1d732492dedc7d9
yaseminbicer <yasemin.bicer@bahcesehir.edu.tr>
2024-03-03 02:15:28 +0300

    Sertifikalarımda ekle, düzenle ve sil işlemleri eklendi

---
+++ MvcCvPrroject/Controllers/SertifikalarimController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvPrroject.Models.Entity;
+using MvcCvPrroject.Repositories;
+
+namespace MvcCvPrroject.Controllers
+{
+    public class SertifikalarimController : Controller
+    {
+        GenericRepository<tblSertifikalarim> repo= new GenericRepository<tblSertifikalarim> ();
+        // GET: Sertifikalarim
+        public ActionResult Index()
+        {
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika =repo.Find(x=> x.ID == id);
+            ViewBag.d = id;
+            return View(sertifika);
+        }
+        [HttpPost]
+        public ActionResult SertifikaGeitr(tblSertifikalarim t)
+        {
+            var sertifika = repo.Find(x => x.ID == t.ID);
+            sertifika.Aciklama = t.Aciklama;
+            sertifika.Tarih=t.Tarih;
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YeniSertifika()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSertifika(tblSertifikalarim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika =repo.Find(x => x.ID == id);
+            repo.TDelete(sertifika);
+            return RedirectToAction("Index");
+        }
+    }
+}
