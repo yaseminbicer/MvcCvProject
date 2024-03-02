1f1b70a48a3e809d8f2729e74283f649747a014d
yaseminbicer <yasemin.bicer@bahcesehir.edu.tr>
2024-03-03 02:57:29 +0300

    Iletisim sayfası eklendi

---
+++ MvcCvPrroject/Controllers/IletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvPrroject.Models.Entity;
+using MvcCvPrroject.Repositories;
+
+namespace MvcCvPrroject.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        GenericRepository<tblIletisim> repo=new GenericRepository<tblIletisim>();
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
