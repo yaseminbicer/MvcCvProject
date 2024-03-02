79b5774a749474fde78151e3ad3e6a8ed0b15ac4
yaseminbicer <yasemin.bicer@bahcesehir.edu.tr>
2024-03-03 00:00:43 +0300

    Egitimlerde ekle, sil ve düzenle işlemleri eklendi

---
+++ MvcCvPrroject/Controllers/EgitimlerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvPrroject.Models.Entity;
+using MvcCvPrroject.Repositories;
+
+namespace MvcCvPrroject.Controllers
+{
+    public class EgitimlerController : Controller
+    {
+        // GET: Egitimler
+        GenericRepository<tblEgitimlerim> repo = new GenericRepository<tblEgitimlerim>();
+        public ActionResult Index()
+        {
+            var egitim =repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(tblEgitimlerim p) 
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim =repo.Find(x=> x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x=> x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(tblEgitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik =t.Baslik;
+            egitim.AltBaslik1 =t.AltBaslik1;
+            egitim.AltBaslik2 =t.AltBaslik2;
+            egitim.Tarih=t.Tarih;
+            egitim.GNO= t.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MvcCvPrroject/Controllers/YeteneklerimController.cs
+++ MvcCvPrroject/Controllers/YeteneklerimController.cs
@@ -27,5 +27,22 @@ namespace MvcCvPrroject.Controllers
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekDuzenle(int id)
+        {
+            var yetenek= repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekDuzenle()
+        {
+            return View();
+        }
     }
 }
